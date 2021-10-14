f3185d1d63058471edf0fc83b8e71b7a0d324f1f
Rteske <ryanteske@outlook.com>
2021-10-14 16:59:54 -0700

    Add friends user stories

---
+++ app/controllers/friends_controller.rb
@@ -0,0 +1,24 @@
+class FriendsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def new
+    @friend = Friend.new
+  end
+
+  def create
+    friend_user = User.find_by email: params[:friend]
+    friend = Friend.new(user_id: session[:user_id], friend_id: friend_user.id)
+    if friend.save
+      flash[:notice] = "Added #{friend_user.username} as a friend"
+    else
+      flash[:alert] = 'Not a valid email'
+    end
+    redirect_to dashboard_index_path
+  end
+
+  private
+
+  def user_params
+    params.permit(:friend)
+  end
+end

56b4e1c302dc2b2c903162c2f2935d59f3619fec
Rteske <ryanteske@outlook.com>
2021-10-13 18:25:39 -0700

    Add Movies Controller

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,13 @@
+class MoviesController < ApplicationController
+  def index
+    if params[:query]
+      @movies = MoviesService.movies_by_name(params[:query])
+    elsif params[:search] = 'top_40'
+      @movies = MoviesService.top_40_movies
+    end
+  end
+
+  def show
+    @movie = MoviesService.movie_details(params[:id])
+  end
+end

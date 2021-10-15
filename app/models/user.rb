26a0d04d262986b72ae904ebe5f60331b26b295f
Rteske <ryanteske@outlook.com>
2021-10-14 17:00:44 -0700

    Rubocop clean up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
-    has_many :events, dependent: :destroy
+  has_many :events, dependent: :destroy
+  has_many :friends
 
-    has_secure_password
+  has_secure_password
 
-    validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Invalid email' }
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Invalid email' }
+
+  def get_friends
+    User.where('users.id IN (?)', friends.pluck(:friend_id))
+  end
 end

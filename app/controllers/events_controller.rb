c9cc18d871bd1c761de2c4072a7b632dcf3eb14d
Ozzie <mikeosmonson@gmail.com>
2021-10-17 09:58:37 -0600

    Add Validations to vaidate event input

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,21 @@
 class EventsController < ApplicationController
   def new
     @event = Event.new
+    @movie = MoviesService.movie_details(params[:movie_id])
   end
 
-  def create; end
+  def create
+    event = Event.new(event_params)
+    if event.save
+      redirect_to dashboard_path, success: 'Party Created Successfully!'
+    else
+      redirect_to new_event_path, alert: 'Party not Created: Please re-enter information'
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :starttime, :duration).merge(user_id: current_user.id)
+  end
 end

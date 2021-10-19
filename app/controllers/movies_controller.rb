1241e37d7d976f793450df41c03ab1e67370c142
Rteske <ryanteske@outlook.com>
2021-10-19 12:45:05 -0700

    Refactor Movies service calls to Movies Facade calls

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,9 @@
 class MoviesController < ApplicationController
   def index
     if params[:query]
-      @movies = MoviesService.movies_by_name(params[:query])
-    elsif params[:search] = 'top_40'
-      @movies = MoviesService.top_40_movies
+      @movies = MovieFacade.movies_query(params[:query])
+    elsif params[:search] == 'top_40'
+      @movies = MovieFacade.top40
     end
   end
 end

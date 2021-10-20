5a5d4b54fe050236330262c36f41a9217b655fce
Rteske <ryanteske@outlook.com>
2021-10-20 16:52:47 -0700

    Modify tests and views to accept user_present method

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root 'welcome#show'
   get 'sign_in', to: 'sessions#new'
   post 'sign_in', to: 'sessions#create'
+  get 'sign_out', to: 'sessions#delete'
+  delete 'sign_out', to: 'sessions#destroy'
+
 
   resource :users, only: :create
   get 'dashboard', controller: :users, action: :show

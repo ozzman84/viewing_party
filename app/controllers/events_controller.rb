7679b43311df07da1fda74114c36ff93406b588b
Rteske <ryanteske@outlook.com>
2021-10-19 23:27:48 -0700

    Fix Stopping Point Bug with rendering new event form partial

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,23 +1,23 @@
 class EventsController < ApplicationController
+  before_action :set_current_movie
+
   def new
     @event = Event.new
-    @movie = MovieFacade.movie_details(params[:movie_id])
+    @movie = MovieFacade.movie_details(session[:movie_id])
     @friends = current_user.get_friends
     @event.attendees.build
   end
 
   def create
+    @event = Event.new(event_params.merge({ user_id: current_user.id }))
+    @movie = MovieFacade.movie_details(session[:movie_id])
     @friends = current_user.get_friends
-    @movie = MovieFacade.movie_details(movie_params[:movie_id])
-    @event = Event.new
-    @event.attendees.build
-    event = Event.new(event_params.merge({ user_id: current_user.id }))
-
-    if event.save
+    if @event.save
+      session[:movie_id] = nil
       redirect_to dashboard_path, success: 'It\'s the f*#@in Catalina Wine Mixer! It\'s party time!'
     else
-      flash[:notice] = event.errors.messages[:starttime]
-      render(partial: "shared/new_event_form", locals: { event: @event, movie: @movie, friends: @friends })#, alert: "That's a bold move #{current_user.username}, let's see if it pays off. Party not Created: Please re-enter information"
+      flash[:notice] = @event.errors.messages[:starttime]
+      render(partial: 'shared/new_event_form', layout: 'layouts/application', locals: { event: @event, movie: @movie, friends: @friends, attendees: @event.attendees.build })#, alert: "That's a bold move #{current_user.username}, let's see if it pays off. Party not Created: Please re-enter information"
     end
   end
 
@@ -30,4 +30,9 @@ class EventsController < ApplicationController
   def movie_params
     params.permit(:movie_id)
   end
+
+  def set_current_movie
+    session[:movie_id] ||= params[:movie_id]
+  end
+
 end

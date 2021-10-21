fe14634877129d371759d352753aa5cf1e26cd57
Rteske <ryanteske@outlook.com>
2021-10-20 21:54:20 -0700

    Refactor bug Movies index page search

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,9 @@
 class MoviesController < ApplicationController
   def index
     if params[:query]
-      @movies = MovieFacade.movies_query(params[:query])
+      unless params[:query] == ""
+        @movies = MovieFacade.movies_query(params[:query])
+      end
     elsif params[:search] == 'top_40'
       @movies = MovieFacade.top40
     end
